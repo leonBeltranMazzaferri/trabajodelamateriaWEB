4b32a016f10b2c9024244d022ed14f000709a075
leonbeltranmazzaferri <leonbeltranm07@gmail.com>
2025-05-30 16:15:23 -0300

    nav terminao

--- codigo/modulos.js
+++ codigo/modulos.js
@@ -3,7 +3,7 @@ document.querySelector('#header').innerHTML = `
     <button id="searchBtn"><img src="images/search-alt.png" alt="lupa"></button>
     <button id="cartBtn"><img src="images/cart.png" alt="carrito"></button>
   </div>
-  <h1 class="titulo">suo</h1>
+  <h1 class="titulo">SUO</h1>
   <div class="header-right">
     <button id="menuBtn"><img src="images/menu-close.png" alt="menú"></button>
   </div>
@@ -11,10 +11,8 @@ document.querySelector('#header').innerHTML = `
 
 document.querySelector('#nav').innerHTML = `
   <ul>
-    <li>UNO 1</li>
-    <li>DOS 2</li>
-    <li>TRES 3</li>
-    <li>CUATRO</li>
+    <li>link 1</li>
+    <li>link 2</li>
   </ul>
 `;
 
