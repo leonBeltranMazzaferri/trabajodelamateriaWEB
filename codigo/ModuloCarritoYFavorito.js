7c51cb2747ce407877d6babe16e8b4b1dc5b0a1e
Margarita Martínez <martinezcabral.margarita@tecnica7.edu.ar>
2025-08-27 17:20:27 -0300

    estilos en los botones de los producos

--- codigo/ModuloCarritoYFavorito.js
+++ codigo/ModuloCarritoYFavorito.js
@@ -1,5 +1,4 @@
 
-// Manejo de carrito y favoritos
 
 let carrito = [];
 let favoritos = [];
@@ -24,5 +23,32 @@ function guardarCarrito() {
 
 function guardarFavoritos() {
   localStorage.setItem('favoritos', JSON.stringify(favoritos));
+}
+
+function actualizarCantidad(id) {
+  const input = document.getElementById(`cantidad-${id}`);
+  let nuevaCantidad = parseInt(input.value);
+  if (isNaN(nuevaCantidad) || nuevaCantidad < 1) nuevaCantidad = 1;
+
+  const producto = carrito.find(p => Number(p.id) === Number(id));
+  if (producto) {
+    producto.cantidad = nuevaCantidad;
+    guardarCarrito();
+    mostrarCarrito();
+  }
+}
+
+function mostrarCarrito() {
+  const container = document.getElementById("carrito-container");
+  container.innerHTML = "<h2>Carrito</h2>";
+
+  if (carrito.length === 0) {
+    container.innerHTML += "<p>El carrito está vacío.</p>";
+    return;
+  }
+
+  carrito.forEach(p => {
+   
+  });
 }
 
